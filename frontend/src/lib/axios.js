968fbeddf5985ec9284aba138edd4a337aa41208
DEVANGBAJPAI23 <devangbajpai69@gmail.com>
2025-03-28 15:46:33 +0530

    changes

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
-
 export const axiosInstance = axios.create({
-    baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
-    withCredentials: true,
-  });
+  baseURL:
+    import.meta.env.MODE === "development"
+      ? "https://chit-chat-4olf.onrender.com/api"
+      : "/api",
+  withCredentials: true,
+});
